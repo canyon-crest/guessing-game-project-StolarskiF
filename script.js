7bb35c113c8f3c293685faebf0ef7fd93656de22
StolarskiF <stolarskif5306@my.sduhsd.net>
2025-11-06 08:44:44 +0000

    Draft 4 Vid 2 Final

--- script.js
+++ script.js
@@ -33,17 +33,43 @@ function play(){
 
 function makeGuess(){
     let userGuess = parseInt(guess.value);
-    if(isNaN(userGuess)  || userGuess == ""){
+    if(isNaN(userGuess) || userGuess == ""){
         msg.innerHTML = "Invalid. Guess a #1-" + level;
         return;
     }
     score++;
-    if(userGuess < answser)
+    if(userGuess < answer)
         msg.innerHTML = "Too low, guess a #1-" + level;
     else if(userGuess > answer)
         msg.innerHTML = "Too high, guess a #1-" + level;
     else{
         msg.innerHTML = "Correct! You win, it took " + score + " tries.";
+        scoreArr.push(score);
+        updateScore();
+        reset();
     }
+}
+function updateScore(){
+    wins.innerHTML = "Total wins: " + scoreArr.length;
+    let lb = document.getElementsByName("leaderboard")
+    scoreArr.sort((a,b) => a-b);
+    let sum = 0;
 
+    for(let i=0; i<scoreArr.length; i++){
+        if(i < lb.length){
+            lb[i].innerHTML = scoreArr [i];
+        }
+        sum += scoreArr[i];
+    }
+    let avg = sum/scoreArr.length;
+    avgScore.innerHTML = "Average Score: " + avg.toFixed(2);
+}
+function reset(){
+    guess.disabled = true;
+    guessBtn.disabled = true;
+    playBtn.disabled = false;
+    for(let i=0; i<levelArr.length;i++)
+        levelArr[i].disabled = false;
+    guess.value = "";
+    guess.placeholder = "";
 }
